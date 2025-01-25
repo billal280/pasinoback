6ce04221370f64e1f0b75b08509b2fea334d1649
Billal Saidi <billal.saidi136@gmail.com>
2025-01-25 15:53:08 +0100

    first commit

---
+++ src/main/java/com/pasinoback/pasinoback/PasinobackApplication.java
@@ -0,0 +1,13 @@
+package com.pasinoback.pasinoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasinobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasinobackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pasinoback/pasinoback/PasinobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasinoback.pasinoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasinobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
